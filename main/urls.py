c5fbcde89dfa50e21d2926c957f9d8aeec35a733
wsnugroho <wisnunugroho614@gmail.com>
2024-09-18 04:39:57 +0700

    fix: xml & json url path (with trailing slash)

--- main/urls.py
+++ main/urls.py
@@ -8,8 +8,8 @@ app_name = "main"
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("create-product", create_product, name="create_product"),
-    path("xml", show_xml, name="show_xml"),
-    path("xml/<str:id>", show_xml_by_id, name="show_xml_by_id"),
-    path("json", show_json, name="show_json"),
-    path("json/<str:id>", show_json_by_id, name="show_json_by_id"),
+    path("xml/", show_xml, name="show_xml"),
+    path("xml/<str:id>/", show_xml_by_id, name="show_xml_by_id"),
+    path("json/", show_json, name="show_json"),
+    path("json/<str:id>/", show_json_by_id, name="show_json_by_id"),
 ]

b9730c36b44bb3e4ffc2d7a499bace39ed7e3b73
wsnugroho <wisnunugroho614@gmail.com>
2024-09-18 04:37:44 +0700

    feat(tugas3): add json endpoints

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from main.views import create_product, show_main, show_xml, show_xml_by_id
+from main.views import (create_product, show_json, show_json_by_id, show_main,
+                        show_xml, show_xml_by_id)
 
 app_name = "main"
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path("create-product", create_product, name="create_product"),
     path("xml", show_xml, name="show_xml"),
     path("xml/<str:id>", show_xml_by_id, name="show_xml_by_id"),
+    path("json", show_json, name="show_json"),
+    path("json/<str:id>", show_json_by_id, name="show_json_by_id"),
 ]
--- main/views.py
+++ main/views.py
@@ -37,3 +37,17 @@ def show_xml_by_id(request, id):
     return HttpResponse(
         serializers.serialize("xml", data), content_type="application/xml"
     )
+
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
+
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )

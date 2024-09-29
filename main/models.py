510631c7569f4eaec8eb6c60883360cec42442d4
wsnugroho <wisnunugroho614@gmail.com>
2024-09-29 21:30:40 +0700

    fix: change price to only accept positive integer

--- main/models.py
+++ main/models.py
@@ -8,5 +8,5 @@ class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.PositiveIntegerField()
     description = models.TextField()

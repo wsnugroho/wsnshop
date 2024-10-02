d37ecfc695c580b6db76894367bb4601bae3b3be
wsnugroho <wisnunugroho614@gmail.com>
2024-10-02 07:44:09 +0700

    feat(tugas5): static file configuration

--- wsnshop/settings.py
+++ wsnshop/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -130,6 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static"  # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = (
+        BASE_DIR / "static"  # merujuk ke /static root project pada mode production
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

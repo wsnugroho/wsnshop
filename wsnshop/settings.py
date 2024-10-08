6adafc46523bff20df469ba0ec07de69992a7b05
Wisnu Nugroho <wisnunugroho614@gmail.com>
2024-10-09 06:44:58 +0700

    chore: fixing import os

--- wsnshop/settings.py
+++ wsnshop/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
-
-from django.conf import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,9 +135,7 @@ if DEBUG:
         BASE_DIR / "static"  # merujuk ke /static root project pada mode development
     ]
 else:
-    STATIC_ROOT = (
-        BASE_DIR / "static"  # merujuk ke /static root project pada mode production
-    )
+    STATIC_ROOT = BASE_DIR / "static"  # merujuk ke /static root project pada mode production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

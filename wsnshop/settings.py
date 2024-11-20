20febddb4b0e157448b187f2f7c244e32a66020e
Wisnu Nugroho <wisnunugroho614@gmail.com>
2024-11-20 09:07:34 +0700

    feat(tugas9): configure for flutter endpoints

--- wsnshop/settings.py
+++ wsnshop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "wisnu-nugroho31-wsnshop.pbp.cs.ui.ac.id",
+    "10.0.2.2",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "main",
     "authentication",
 ]
@@ -62,6 +64,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "wsnshop.urls"
@@ -142,3 +145,10 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'

96f59fa5866467db5d5d50b5c45d8cf140368bc1
Wisnu Nugroho <wisnunugroho614@gmail.com>
2024-11-20 09:10:18 +0700

    feat(tugas9): add login endpoints

--- wsnshop/urls.py
+++ wsnshop/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("authentication.urls")),
     path("", include("main.urls")),
-    path("", include("authentication.urls"))
 ]
